82df00bec7b34d987c72842f05c921ffd97377ba
Kritika Ranjan <kritikaranjan28@gmail.com>
2021-07-26 13:05:39 +0530

    Update Image_scraping2.py

--- Web-Scraper/Image_scraping2.py
+++ Web-Scraper/Image_scraping2.py
@@ -1,16 +1,14 @@
-# Using urllib and BeautifulSoup
-# Import module
-# read URL with urlopen()
-# Pass the requests into a BeautifulSoup() function
-# Use 'img' tag to find them all tag ('src')
-
-
-
+# Importing module urllib and BeautifulSoup
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+# Reading URL with urlopen()
 data = urlopen('https://www.briantracy.com/blog/personal-success/26-motivational-quotes-for-success/')
+
+# Passing the requests into a BeautifulSoup() function
 soup = BeautifulSoup(data, 'html.parser')
+
+# Using 'img' tag to find them all tag ('src')
 images = soup.find_all('img')
  
 for item in images:
